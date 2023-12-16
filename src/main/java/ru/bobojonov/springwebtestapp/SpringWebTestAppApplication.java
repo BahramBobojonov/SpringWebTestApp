fd72c087298b12a75e5bad18df77f0b94ae6ff9d
vivar <bobojonoffff@gmail.com>
2023-12-17 01:25:32 +0300

    init commit

---
+++ src/main/java/ru/bobojonov/springwebtestapp/SpringWebTestAppApplication.java
@@ -0,0 +1,13 @@
+package ru.bobojonov.springwebtestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bobojonov/springwebtestapp/SpringWebTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bobojonov.springwebtestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
